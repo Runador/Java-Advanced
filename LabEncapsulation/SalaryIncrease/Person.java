362923fe5ee04ac01c7e9ec3d1a6b4fd02646b4d
Runador <runador123456@gmail.com>
2024-05-02 03:15:36 +0300

    Adding Main.java

--- LabEncapsulation/SalaryIncrease/Person.java
+++ LabEncapsulation/SalaryIncrease/Person.java
@@ -1,10 +1,6 @@
 package LabEncapsulation.SalaryIncrease;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.DecimalFormat;
 
 public class Person {
 
@@ -54,32 +50,17 @@ public class Person {
 
     @Override
     public String toString() {
-        return String.format("%s %s gets %f leva", getFirstName(), getLastName(), getSalary());
+        return String.format("%s %s gets %s leva", getFirstName(), getLastName(),
+                new DecimalFormat("#.0####").format(this.getSalary()));
     }
 
     public void increaseSalary(double bonus) {
         double percent = (this.salary * bonus) / 100;
-        if (getAge() < 30) {
-            this.salary = this.salary + (percent / 2);
+        if (this.getAge() < 30) {
+            this.setSalary(this.salary + (percent / 2));    // нужно использовать сеттер
+        } else {
+            this.setSalary(this.salary += percent);         // нужно использовать сеттер
         }
-        this.salary += percent;
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        List<Person> people = new ArrayList<>();
-        int n = Integer.parseInt(reader.readLine());
-
-        for (int i = 0; i < n; i++) {
-            String[] input = reader.readLine().split("\\s+");
-            Person person = new Person(input[0], input[1], Integer.parseInt(input[2]), Double.parseDouble(input[3]));
-            people.add(person);
-        }
-        double bonus = Double.parseDouble(reader.readLine());
-        for (Person person : people) {
-            person.increaseSalary(bonus);
-            System.out.println(person);
-        }
-
-    }
 }
