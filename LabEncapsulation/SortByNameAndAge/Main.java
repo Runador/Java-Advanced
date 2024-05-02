8bf55569b97dd2ecd297a46d8b49f191428b29e3
Runador <runador123456@gmail.com>
2024-05-02 03:15:18 +0300

    Adding Main.java

---
+++ LabEncapsulation/SortByNameAndAge/Main.java
@@ -0,0 +1,39 @@
+package LabEncapsulation.SortByNameAndAge;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<Person> personList = new ArrayList<>();
+        int n = Integer.parseInt(reader.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().split("\\s+");
+            personList.add(new Person(input[0], input[1], Integer.parseInt(input[2])));
+        }
+
+        Collections.sort(personList, (firstPerson, secondPerson) -> {
+            int sComp = firstPerson.getFirstName().compareTo(secondPerson.getFirstName());
+
+            if (sComp != 0) {
+                return sComp;
+            } else {
+                return Integer.compare(firstPerson.getAge(), secondPerson.getAge());
+            }
+        });
+
+        for (Person person : personList) {
+            System.out.println(person);
+        }
+
+    }
+
+}
--- LabEncapsulation/SortByNameAndAge/Person.java
+++ LabEncapsulation/SortByNameAndAge/Person.java
@@ -1,12 +1,5 @@
 package LabEncapsulation.SortByNameAndAge;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Person {
 
     private String firstName;
@@ -48,30 +41,4 @@ public class Person {
         return String.format("%s %s is %d years old.", getFirstName(), getLastName(), getAge());
     }
 
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        List<Person> personList = new ArrayList<>();
-        int n = Integer.parseInt(reader.readLine());
-
-        for (int i = 0; i < n; i++) {
-            String[] input = reader.readLine().split("\\s+");
-            personList.add(new Person(input[0], input[1], Integer.parseInt(input[2])));
-        }
-
-        Collections.sort(personList, (firstPerson, secondPerson) -> {
-            int sComp = firstPerson.getFirstName().compareTo(secondPerson.getFirstName());
-
-            if (sComp != 0) {
-                return sComp;
-            } else {
-                return Integer.compare(firstPerson.getAge(), secondPerson.getAge());
-            }
-        });
-
-        for (Person person : personList) {
-            System.out.println(person);
-        }
-
-    }
 }
