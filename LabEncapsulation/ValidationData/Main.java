b5291bf4f5e31214acf4cf5c5b42fd22f8fa51dc
Runador <runador123456@gmail.com>
2024-05-03 20:02:33 +0300

    The file Main.java has been modified

--- LabEncapsulation/ValidationData/Main.java
+++ LabEncapsulation/ValidationData/Main.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException { // кидаем IOException
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
