b8868a226430e999a4760b0588afac275183a0f7
Runador <runador123456@gmail.com>
2024-05-06 23:02:49 +0300

    Adding SimpleCalculator2.java

---
+++ StacksAndQueuesLab/SimpleCalculator2.java
@@ -0,0 +1,34 @@
+package StacksAndQueuesLab;
+
+import java.util.ArrayDeque;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class SimpleCalculator2 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // 2 + 3 - 1
+
+        ArrayDeque<String> stack = new ArrayDeque();
+        String[] tokens = scanner.nextLine().split("\\s+");
+        Collections.addAll(stack, tokens);
+
+        int result;
+
+        while (stack.size() > 1) {
+            int firstNumber = Integer.parseInt(stack.pop());
+            String operator = stack.pop();
+            int secondNumber = Integer.parseInt(stack.pop());
+            if (operator.equals("+")) {
+                result = firstNumber + secondNumber;
+            } else {
+                result = firstNumber - secondNumber;
+            }
+            stack.push(String.valueOf(result));
+        }
+
+        System.out.println(stack.peek());
+    }
+}
