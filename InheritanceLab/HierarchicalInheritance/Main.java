8ef1dbd98dab246caea4a5a9e8f3cf694c423b5f
Runador <runador123456@gmail.com>
2024-05-13 13:48:17 +0300

    Adding InheritanceLab/HierarchicalInheritance

---
+++ InheritanceLab/HierarchicalInheritance/Main.java
@@ -0,0 +1,17 @@
+package InheritanceLab.HierarchicalInheritance;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cat cat = new Cat();
+        cat.eat();
+        cat.meow();
+
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+
+    }
+
+}
