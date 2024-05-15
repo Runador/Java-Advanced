2f7cec27d6a8fd532c04b866ecab59b371b832c0
Runador <runador123456@gmail.com>
2024-05-15 21:21:40 +0300

    Adding SimpleTextEditor.java

---
+++ ExercisesStacksAndQueues/SimpleTextEditor.java
@@ -0,0 +1,46 @@
+package ExercisesStacksAndQueues;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class SimpleTextEditor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> stack = new ArrayDeque<>();
+
+        StringBuilder text = new StringBuilder();
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+
+            String[] tokens = scanner.nextLine().split("\\s+");
+            String command = tokens[0];
+
+            switch (command) {
+                case "1" -> {
+                    String textToAdd = tokens[1];
+                    text.append(textToAdd);
+                    stack.push(text.toString());
+                }
+                case "2" -> {
+                    int countEraseSymbols = Integer.parseInt(tokens[1]);
+                    text.delete(text.length() - countEraseSymbols, text.length());
+                    stack.push(text.toString());
+                }
+                case "3" -> {
+                    int index = Integer.parseInt(tokens[1]);
+                    char element = text.charAt(index - 1);
+                    System.out.println(element);
+                }
+                case "4" -> {
+                    stack.pop();
+                    assert stack.peek() != null;
+                    text = new StringBuilder(stack.peek());
+                }
+            }
+        }
+
+    }
+}
