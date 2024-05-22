9df5c8f618ac1cb8410309fd495232f3ab389eea
Runador <runador123456@gmail.com>
2024-05-22 17:46:04 +0300

    Modifying AverageStudentsGrades.java

--- SetsAndMapsAdvancedLab/AverageStudentsGrades.java
+++ SetsAndMapsAdvancedLab/AverageStudentsGrades.java
@@ -1,9 +1,6 @@
 package setsAndMapsAdvancedLab;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 public class AverageStudentsGrades {
     public static void main(String[] args) {
@@ -14,17 +11,25 @@ public class AverageStudentsGrades {
         int n = Integer.parseInt(scanner.nextLine());
 
         while (n-- > 0) {
-
             String[] tokens = scanner.nextLine().split(" ");
             String name = tokens[0];
             double grade = Double.parseDouble(tokens[1]);
 
             studentsGradesMap.putIfAbsent(name, new ArrayList<>());
             studentsGradesMap.get(name).add(grade);
-            /*if (studentsGradesMap.containsKey(name)) {
-                studentsGradesMap.put(name, studentsGradesMap.get(name).add(grade));
-            }*/
+        }
 
+        for (Map.Entry<String, List<Double>> entry : studentsGradesMap.entrySet()) {
+            double avg;
+            double sum = 0;
+
+            for (double grade : entry.getValue()) {
+                sum += grade;
+            }
+            avg = sum / entry.getValue().size();
+            System.out.printf("%s -> ", entry.getKey());
+            entry.getValue().forEach(grade -> System.out.printf("%.2f ", grade));
+            System.out.printf("(avg: %.2f)%n", avg);
         }
 
     }
