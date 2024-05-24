07e5618a76016c174758cc3b173c5379c9c1b828
Runador <runador123456@gmail.com>
2024-05-24 23:50:40 +0300

    Adding ReadFile.java

---
+++ StreamsFilesAndDirectoriesLab/ReadFile.java
@@ -0,0 +1,22 @@
+package StreamsFilesAndDirectoriesLab;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class ReadFile {
+    public static void main(String[] args) {
+
+        String path = "C:\\Users\\Runador\\Documents\\Java Advanced\\Java-Advanced\\StreamsFilesAndDirectoriesLab\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\Files-and-Streams\\input.txt";
+
+        try (FileInputStream inputStream = new FileInputStream(path)){
+            int oneByte = inputStream.read();
+            while (oneByte >= 0) {
+                System.out.printf("%s ", Integer.toBinaryString(oneByte));
+                oneByte = inputStream.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
