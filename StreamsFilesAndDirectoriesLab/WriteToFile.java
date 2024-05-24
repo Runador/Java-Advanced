f9ede921c62abaf4bc12700e8c7b78657cee27d2
Runador <runador123456@gmail.com>
2024-05-24 23:50:27 +0300

    Adding WriteToFile.java

---
+++ StreamsFilesAndDirectoriesLab/WriteToFile.java
@@ -0,0 +1,21 @@
+package StreamsFilesAndDirectoriesLab;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class WriteToFile {
+    public static void main(String[] args) {
+        String path = "C:\\Users\\Runador\\Documents\\Java Advanced\\Java-Advanced\\StreamsFilesAndDirectoriesLab\\04. Java-Advanced-Files-and-Streams-Lab-Resources\\Files-and-Streams\\input.txt";
+
+        try (FileInputStream inputStream = new FileInputStream(path)){
+            int oneByte = inputStream.read();
+            while (oneByte >= 0) {
+                if (!)
+                System.out.printf("%s ", Integer.toBinaryString(oneByte));
+                oneByte = inputStream.read();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
