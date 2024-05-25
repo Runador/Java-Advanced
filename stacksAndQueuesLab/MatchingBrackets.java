fd0ff49d6f107903ad361348f16b8387bc24d43b
Runador <runador123456@gmail.com>
2024-05-25 21:13:09 +0300

    Modifying MatchingBrackets.java

--- stacksAndQueuesLab/MatchingBrackets.java
+++ stacksAndQueuesLab/MatchingBrackets.java
@@ -7,31 +7,24 @@ public class MatchingBrackets {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayDeque<String> stack = new ArrayDeque<>();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
 
         String expression = scanner.nextLine();
 
         //  1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5
-
-        //  (2 + 3)
-        //  (3 + 1)
-        //  (2 - (2 + 3) * 4 / (3 + 1))
-
-        int firstIndex = 0;
-        int secondIndex = 0;
 
         for (int i = 0; i < expression.length(); i++) {
 
             char symbol = expression.charAt(i);
 
             if (symbol == '(') {
-                firstIndex = Integer.parseInt(String.valueOf(i));
+                int firstIndex = Integer.parseInt(String.valueOf(i));
+                stack.push(firstIndex);
             } else if (symbol == ')') {
-                secondIndex = Integer.parseInt(String.valueOf(i));
+                int firstIndex = stack.pop();
+                int secondIndex = i;
                 String substring = expression.substring(firstIndex, secondIndex + 1);
-                stack.push(substring);
-                firstIndex = 0;
-                secondIndex = 0;
+                System.out.println(substring);
             }
 
         }
