0e3b896fce233a977791c416412b57ad786fbf8e
Runador <runador123456@gmail.com>
2024-05-30 23:59:20 +0300

    Adding CarInfo/Main.java

---
+++ labDefiningClasses/CarInfo/Main.java
@@ -0,0 +1,24 @@
+package labDefiningClasses.CarInfo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        while (n-- > 0) {
+
+
+            String[] tokens = scanner.nextLine().split("\\s+");
+
+            String brand = tokens[0];
+            String model = tokens[1];
+            int horsePower = Integer.parseInt(tokens[2]);
+            Car car = new Car(brand, model, horsePower);
+            System.out.println(car);
+        }
+    }
+}
