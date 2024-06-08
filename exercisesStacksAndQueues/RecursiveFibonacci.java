dea0f96bde02df6b8cb2df9f99839e2e47d98ab9
Runador <runador123456@gmail.com>
2024-06-08 23:47:10 +0300

    Modifying RecursiveFibonacci.java

--- exercisesStacksAndQueues/RecursiveFibonacci.java
+++ exercisesStacksAndQueues/RecursiveFibonacci.java
@@ -1,7 +1,22 @@
 package exercisesStacksAndQueues;
 
+import java.util.Scanner;
+
 public class RecursiveFibonacci {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+
+        System.out.println(getFibonacci(n));
+    }
+
+    public static long getFibonacci(int n) {
+
+        if (n <= 1) {
+            return 1;
+        }
+
+        return getFibonacci(n - 1) + getFibonacci(n - 2);
     }
 }
--- exercisesStacksAndQueues/Robotics.java
+++ exercisesStacksAndQueues/Robotics.java
@@ -2,6 +2,6 @@ package exercisesStacksAndQueues;
 
 public class Robotics {
     public static void main(String[] args) {
-        
+
     }
 }
