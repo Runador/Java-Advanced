cece0a57d05b3a64811f64d373cb40b9ac0c259f
Runador <runador123456@gmail.com>
2024-06-09 00:00:02 +0300

    Modifying Fibonacci

--- exercisesStacksAndQueues/RecursiveFibonacci.java
+++ exercisesStacksAndQueues/RecursiveFibonacci.java
@@ -3,20 +3,32 @@ package exercisesStacksAndQueues;
 import java.util.Scanner;
 
 public class RecursiveFibonacci {
+
+    private static long[] fibonacciMemory;
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         int n = Integer.parseInt(scanner.nextLine());
+        fibonacciMemory = new long[n + 1];
 
-        System.out.println(getFibonacci(n));
+        long fibonacci = getFibonacci(n);
+        System.out.println(fibonacci);
     }
 
     public static long getFibonacci(int n) {
 
         if (n <= 1) {
             return 1;
+        } else {
+            if (fibonacciMemory[n] != 0) {
+                return fibonacciMemory[n];
+            }
         }
 
-        return getFibonacci(n - 1) + getFibonacci(n - 2);
+        long fib = getFibonacci(n - 1) + getFibonacci(n - 2);
+        fibonacciMemory[n] = fib;
+
+        return fib;
     }
 }
