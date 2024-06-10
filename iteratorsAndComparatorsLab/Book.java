2a6addafb563a70f12ed431dad393d51811b6d1c
Runador <runador123456@gmail.com>
2024-06-10 23:10:07 +0300

    Adding Book.java

---
+++ iteratorsAndComparatorsLab/Book.java
@@ -0,0 +1,11 @@
+package iteratorsAndComparatorsLab;
+
+import java.util.Scanner;
+
+public class Book {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
