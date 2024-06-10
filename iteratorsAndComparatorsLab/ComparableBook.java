575f5de327db7a329786cbf1cf32d6d4c049dc39
Runador <runador123456@gmail.com>
2024-06-10 23:10:46 +0300

    Adding ComparableBook.java

---
+++ iteratorsAndComparatorsLab/ComparableBook.java
@@ -0,0 +1,11 @@
+package iteratorsAndComparatorsLab;
+
+import java.util.Scanner;
+
+public class ComparableBook {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
