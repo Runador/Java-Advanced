e1a15bd47d8fd445061fb611da41d48289d5f6d5
Runador <runador123456@gmail.com>
2024-06-14 23:55:00 +0300

    Adding FishingCompetition.java

---
+++ examPreparation14June2024/FishingCompetition.java
@@ -0,0 +1,12 @@
+package examPreparation14June2024;
+
+import java.util.Scanner;
+
+public class FishingCompetition {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+}
