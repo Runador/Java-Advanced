c19673e4365173ec95485329c2f323bb1f5946c0
Runador <runador123456@gmail.com>
2024-06-19 23:59:37 +0300

    Adding parots

---
+++ parrots/Cage.java
@@ -0,0 +1,86 @@
+package parrots;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cage {
+
+    private String name;
+    private int capacity;
+    private List<Parrot> data;
+
+    public Cage(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+        data = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public List<Parrot> getData() {
+        return data;
+    }
+
+    public void setData(List<Parrot> data) {
+        this.data = data;
+    }
+
+    public void add(Parrot parrot) {
+        if(data.size() < capacity) {
+            data.add(parrot);
+        }
+    }
+
+    public boolean remove(String name) {
+        return data.removeIf(parrot -> parrot.getName().equals(name));
+    }
+
+    public Parrot sellParrot(String name) {
+        for (Parrot parrot : data) {
+            if (data.contains(parrot)) {
+                if (parrot.getName().equals(name)) {
+                    parrot.setAvailable(false);
+                    return parrot;
+                }
+            }
+        }
+        return null;
+    }
+
+    public List<Parrot> sellParrotBySpecies(String species) {
+        List<parrots.Parrot> toReturn = new ArrayList<>();
+        this.data.forEach(parrot -> {
+            if (parrot.getSpecies().equals(species)) {
+                parrot.setAvailable(false);
+                toReturn.add(parrot);
+            }
+        });
+        return toReturn;
+    }
+
+    public int count() {
+        return this.data.size();
+    }
+
+    public String report() {
+        StringBuilder output = new StringBuilder();
+        output.append("Parrots available at ").append(getName()).append(":").append(System.lineSeparator());
+        data.forEach(parrot -> output.append(parrot).append(System.lineSeparator()));
+        return output.toString().trim();
+    }
+
+}
