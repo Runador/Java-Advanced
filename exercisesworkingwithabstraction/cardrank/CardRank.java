4b08f532954ffb0314ff2dddd18dcb11f44a7764
Runador <runador123456@gmail.com>
2024-06-26 01:16:56 +0300

    Adding CardRank.java

---
+++ exercisesworkingwithabstraction/cardrank/CardRank.java
@@ -0,0 +1,17 @@
+package exercisesworkingwithabstraction.cardrank;
+
+public enum CardRank {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+}
