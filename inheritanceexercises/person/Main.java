6298111b771f8b2b88244795ef0520553901290a
Runador <runador123456@gmail.com>
2024-07-03 01:30:53 +0300

    Adding person package

---
+++ inheritanceexercises/person/Main.java
@@ -0,0 +1,13 @@
+package person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Sergey", 32);
+        Person child = new Child("Ivan", 5);
+
+        System.out.println(person.getAge());
+        System.out.println(person.getName());
+        System.out.println(child.getAge());
+        System.out.println(child.getName());
+    }
+}
