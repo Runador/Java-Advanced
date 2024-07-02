96bd96f50468d02383cc72537a20631dc440d057
Runador <runador123456@gmail.com>
2024-07-02 23:32:02 +0300

    Adding singleinheritance package

---
+++ inheritancelab/singleinheritance/Animal.java
@@ -0,0 +1,8 @@
+package inheritancelab.singleinheritance;
+
+public class Animal {
+
+    public void eat() {
+        System.out.println("eating…");
+    }
+}
---
+++ inheritancelab/singleinheritance/Main.java
@@ -0,0 +1,9 @@
+package inheritancelab.singleinheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
