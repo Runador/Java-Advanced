bc505e569a106f2671da26ef158f1bb717553d94
Runador <runador123456@gmail.com>
2024-07-25 23:49:04 +0300

    Adding stacksAndQueuesLab/HotPotato2.java

---
+++ stacksAndQueuesLab/HotPotato2.java
@@ -0,0 +1,12 @@
+package stacksAndQueuesLab;
+
+import java.util.Scanner;
+
+public class HotPotato2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+}
