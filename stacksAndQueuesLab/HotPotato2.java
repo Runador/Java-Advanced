eb65b9139dab2ff76cc9b784123eccfe5ce90529
Runador <runador123456@gmail.com>
2024-07-26 20:08:52 +0300

    Adding MathPotato2.java

--- stacksAndQueuesLab/HotPotato2.java
+++ stacksAndQueuesLab/HotPotato2.java
@@ -6,7 +6,7 @@ public class HotPotato2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        
+
 
     }
 }
---
+++ stacksAndQueuesLab/MathPotato2.java
@@ -0,0 +1,4 @@
+package stacksAndQueuesLab;
+
+public class MathPotato2 {
+}
