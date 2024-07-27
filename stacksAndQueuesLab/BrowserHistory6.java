08753d2cf6bfa1a5faa6845e65e073d95f4bfafe
Runador <runador123456@gmail.com>
2024-07-27 22:48:13 +0300

    Adding BrowserHistory6.java

---
+++ stacksAndQueuesLab/BrowserHistory6.java
@@ -0,0 +1,12 @@
+package stacksAndQueuesLab;
+
+import java.util.Scanner;
+
+public class BrowserHistory6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+
+    }
+}
