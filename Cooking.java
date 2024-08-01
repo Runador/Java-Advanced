c6ac2590cdb4ea1620002a845a1724554462719d
Runador <runador123456@gmail.com>
2024-08-02 00:58:18 +0300

    Modifying Cooking.java

--- Cooking.java
+++ Cooking.java
@@ -14,19 +14,11 @@ public class Cooking {
         Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt)
                 .forEach(ingredientsStack::push);
 
-        Map<String, Integer> foodsMap = new LinkedHashMap<>();
-        foodsMap.put("Bread", 25);
-        foodsMap.put("Cake", 50);
-        foodsMap.put("Pastry", 75);
-        foodsMap.put("Fruit Pie", 100);
-
-        Map<String, Integer> finalFoodsMap = new TreeMap<>();
-        finalFoodsMap.put("Bread", 0);
-        finalFoodsMap.put("Cake", 0);
-        finalFoodsMap.put("Pastry", 0);
-        finalFoodsMap.put("Fruit Pie", 0);
-
-        boolean isFailed = false;
+        Map<String, Integer> foodsMap = new TreeMap<>();
+        foodsMap.put("Bread", 0);
+        foodsMap.put("Cake", 0);
+        foodsMap.put("Pastry", 0);
+        foodsMap.put("Fruit Pie", 0);
 
         while (!liquidsQueue.isEmpty() && !ingredientsStack.isEmpty()) {
 
@@ -34,55 +26,44 @@ public class Cooking {
             int ingredient = ingredientsStack.peek();
             int sum = liquid + ingredient;
 
-            for (var entry : foodsMap.entrySet()) {
-                int result = entry.getValue();
-                if (sum == result) {
-                    if (result == 25) {
-                        if (finalFoodsMap.containsKey("Bread")) {
-                            finalFoodsMap.put("Bread", finalFoodsMap.get("Bread") + 1);
-                        } else {
-                            finalFoodsMap.put("Bread", 1);
-                        }
-                    } else if (result == 50) {
-                        if (finalFoodsMap.containsKey("Cake")) {
-                            finalFoodsMap.put("Cake", finalFoodsMap.get("Cake") + 1);
-                        } else {
-                            finalFoodsMap.put("Cake", 1);
-                        }
-                    } else if (result == 75) {
-                        if (finalFoodsMap.containsKey("Pastry")) {
-                            finalFoodsMap.put("Pastry", finalFoodsMap.get("Pastry") + 1);
-                        } else {
-                            finalFoodsMap.put("Pastry", 1);
-                        }
-                    } else if (result == 100) {
-                        if (finalFoodsMap.containsKey("Fruit Pie")) {
-                            finalFoodsMap.put("Fruit Pie", finalFoodsMap.get("Fruit Pie") + 1);
-                        } else {
-                            finalFoodsMap.put("Fruit Pie", 1);
-                        }
-                    }
+            switch (sum) {
+                case 25 -> {
+                    foodsMap.put("Bread", foodsMap.get("Bread") + 1);
                     liquidsQueue.poll();
                     ingredientsStack.pop();
-                    break;
                 }
-
-                if (!foodsMap.containsValue(liquid + ingredient)) {
-                    isFailed = true;
-                    break;
+                case 50 -> {
+                    foodsMap.put("Cake", foodsMap.get("Cake") + 1);
+                    liquidsQueue.poll();
+                    ingredientsStack.pop();
                 }
-            }
-
-            if (isFailed) {
-                liquidsQueue.poll();
-                ingredientsStack.push(ingredientsStack.pop() + 3);
+                case 75 -> {
+                    foodsMap.put("Pastry", foodsMap.get("Pastry") + 1);
+                    liquidsQueue.poll();
+                    ingredientsStack.pop();
+                }
+                case 100 -> {
+                    foodsMap.put("Fruit Pie", foodsMap.get("Fruit Pie") + 1);
+                    liquidsQueue.poll();
+                    ingredientsStack.pop();
+                }
+                default -> {
+                    liquidsQueue.poll();
+                    ingredientsStack.push(ingredientsStack.pop() + 3);
+                }
             }
         }
 
-        if (liquidsQueue.isEmpty() && ingredientsStack.isEmpty()) {
+        int counter = 0;
+
+        for (var entry : foodsMap.entrySet()) {
+            int foodValue = entry.getValue();
+            if (foodValue > 0) {
+                counter++;
+            }
+        }
+        if (counter >= 4) {
             System.out.println("Wohoo! You succeeded in cooking all the food!");
-            System.out.println("Liquids left: none");
-            System.out.print("Ingredients left: none");
         } else {
             System.out.println("Ugh, what a pity! You didn't have enough materials to cook everything.");
         }
@@ -90,14 +71,16 @@ public class Cooking {
             System.out.println("Liquids left: none");
             System.out.print("Ingredients left: ");
             System.out.print(String.join(", ", ingredientsStack.toString().replaceAll("[\\[\\]]", "")));
-        }
-        if (!liquidsQueue.isEmpty() && ingredientsStack.isEmpty()) {
+        } else if (!liquidsQueue.isEmpty() && ingredientsStack.isEmpty()) {
             System.out.println("Ingredients left: none");
             System.out.print("Liquids left: ");
             System.out.print(String.join(", ", liquidsQueue.toString().replaceAll("[\\[\\]]", "")));
+        } else {
+            System.out.println("Liquids left: none");
+            System.out.print("Ingredients left: none");
         }
         System.out.println();
-        for (var entry : finalFoodsMap.entrySet()) {
+        for (var entry : foodsMap.entrySet()) {
             String foodName = entry.getKey();
             int foodValue = entry.getValue();
             System.out.printf("%s: %d%n", foodName, foodValue);
