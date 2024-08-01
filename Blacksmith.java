a069063599ed80a6414806aea085561690c6a63a
Runador <runador123456@gmail.com>
2024-08-02 00:07:08 +0300

    Modifying Blacksmith.java

--- Blacksmith.java
+++ Blacksmith.java
@@ -13,81 +13,46 @@ public class Blacksmith {
         Arrays.stream(scanner.nextLine().split("\\s+")).mapToInt(Integer::parseInt)
                 .forEach(carbonStack::push);
 
-        Map<String, Integer> swordsMap = new LinkedHashMap<>();
-        swordsMap.put("Gladius", 70);
-        swordsMap.put("Shamshir", 80);
-        swordsMap.put("Katana", 90);
-        swordsMap.put("Sabre", 110);
-
-        Map<String, Integer> swordsFinalMap = new TreeMap<>();
-
-        boolean isFailed = false;
+        Map<String, Integer> swordsMap = new TreeMap<>();
+        swordsMap.put("Gladius", 0);
+        swordsMap.put("Shamshir", 0);
+        swordsMap.put("Katana", 0);
+        swordsMap.put("Sabre", 0);
 
         while (!steelQueue.isEmpty() && !carbonStack.isEmpty()) {
 
             int steel = steelQueue.peek();
             int carbon = carbonStack.peek();
 
-            for (var entry : swordsMap.entrySet()) {
-                int resources = entry.getValue();
-                if (steel + carbon == resources) {
-                    if (resources == 70) {
-                        if (swordsFinalMap.containsKey("Gladius")) {
-                            swordsFinalMap.put("Gladius", swordsFinalMap.get("Gladius") + 1);
-                        } else {
-                            swordsFinalMap.put("Gladius", 1);
-                        }
-                        steelQueue.poll();
-                        carbonStack.pop();
-                    }
-                    if (resources == 80) {
-                        if (swordsFinalMap.containsKey("Shamshir")) {
-                            swordsFinalMap.put("Shamshir", swordsFinalMap.get("Shamshir") + 1);
-                        } else {
-                            swordsFinalMap.put("Shamshir", 1);
-                        }
-                        steelQueue.poll();
-                        carbonStack.pop();
-                    }
-                    if (resources == 90) {
-                        if (swordsFinalMap.containsKey("Katana")) {
-                            swordsFinalMap.put("Katana", swordsFinalMap.get("Katana") + 1);
-                        } else {
-                            swordsFinalMap.put("Katana", 1);
-                        }
-                        steelQueue.poll();
-                        carbonStack.pop();
-                    }
-                    if (resources == 110) {
-                        if (swordsFinalMap.containsKey("Sabre")) {
-                            swordsFinalMap.put("Sabre", swordsFinalMap.get("Sabre") + 1);
-                        } else {
-                            swordsFinalMap.put("Sabre", 1);
-                        }
-                        steelQueue.poll();
-                        carbonStack.pop();
-                    }
-                }
-                if (!swordsMap.containsValue(steel + carbon)) {
-                    isFailed = true;
-                    break;
-                }
-            }
-
-            if (isFailed) {
+            if (steel + carbon == 70) {
+                swordsMap.put("Gladius", swordsMap.get("Gladius") + 1);
+                steelQueue.poll();
+                carbonStack.pop();
+            } else if (steel + carbon == 80) {
+                swordsMap.put("Shamshir", swordsMap.get("Shamshir") + 1);
+                steelQueue.poll();
+                carbonStack.pop();
+            } else if (steel + carbon == 90) {
+                swordsMap.put("Katana", swordsMap.get("Katana") + 1);
+                steelQueue.poll();
+                carbonStack.pop();
+            } else if (steel + carbon == 110) {
+                swordsMap.put("Sabre", swordsMap.get("Sabre") + 1);
+                steelQueue.poll();
+                carbonStack.pop();
+            } else {
                 steelQueue.poll();
                 carbonStack.push(carbonStack.pop() + 5);
             }
         }
 
-        boolean areAnySwordsForged = swordsFinalMap.size() > 0;
         int totalNumberOfSwords = 0;
 
-        for (var entry : swordsFinalMap.entrySet()) {
+        for (var entry : swordsMap.entrySet()) {
             totalNumberOfSwords += entry.getValue();
         }
 
-        if (areAnySwordsForged) {
+        if (totalNumberOfSwords > 0) {
             System.out.printf("You have forged %d swords.%n", totalNumberOfSwords);
         } else {
             System.out.println("You did not have enough resources to forge a sword.");
@@ -105,10 +70,12 @@ public class Blacksmith {
             System.out.print(String.join(", ", carbonStack.toString().replaceAll("[\\[\\]]", "")));
         }
         System.out.println();
-        for (var entry : swordsFinalMap.entrySet()) {
+        for (var entry : swordsMap.entrySet()) {
             String swordName = entry.getKey();
             int swordValue = entry.getValue();
-            System.out.printf("%s: %d%n", swordName, swordValue);
+            if (swordValue != 0) {
+                System.out.printf("%s: %d%n", swordName, swordValue);
+            }
         }
     }
 }
