caf6354eae27a201ccad89cde0db207cec464d03
Runador <runador123456@gmail.com>
2024-08-18 20:33:32 +0300

    Adding CustomMinFunction.java

---
+++ CustomMinFunction.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CustomMinFunction {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        Function<List<Integer>, Integer> minFunction =
+                numbersList -> numbersList.stream().min(Integer::compareTo).orElse(null);
+
+        System.out.println(minFunction.apply(numbers));
+    }
+}
