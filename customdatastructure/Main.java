b70a6ee9cf134fd0c6e43b555e00323f69759e0c
Runador <runador123456@gmail.com>
2024-08-27 03:12:48 +0300

    Modifying customdatastructure

--- customdatastructure/Main.java
+++ customdatastructure/Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         SmartArray smartArray = new SmartArray();
+
         smartArray.add(1);
         smartArray.add(2);
         smartArray.add(54);
@@ -12,13 +13,34 @@ public class Main {
 
         smartArray.forEach(e -> System.out.print(e + " "));
         System.out.println();
+
         System.out.println(smartArray.get(4));
-        smartArray.remove(3);
-        smartArray.add(2, 65);
-        /*smartArray.remove(2);
-        smartArray.forEach(System.out::println);
+
+        smartArray.add(2, 37);
+        smartArray.forEach(e -> System.out.print(e + " "));
         System.out.println();
-        System.out.println(smartArray.contains(65));
-        System.out.println(smartArray.contains(2));*/
+        smartArray.add(1, 666);
+        smartArray.forEach(e -> System.out.print(e + " "));
+        System.out.println();
+        System.out.println();
+
+        smartArray.remove(1);
+        smartArray.forEach(e -> System.out.print(e + " "));
+        System.out.println();
+
+        smartArray.add(5, 111);
+        smartArray.forEach(e -> System.out.print(e + " "));
+
+        System.out.println();
+        smartArray.add(1, 34);
+        smartArray.forEach(e -> System.out.print(e + " "));
+
+        System.out.println();
+        smartArray.add(10);
+        smartArray.add(13);
+        System.out.println(smartArray.contains(113));
+        System.out.println(smartArray.contains(111));
+
+        smartArray.add(10, 12);
     }
 }
--- customdatastructure/SmartArray.java
+++ customdatastructure/SmartArray.java
@@ -4,13 +4,16 @@ import java.util.function.Consumer;
 
 public class SmartArray {
 
+
     private static final int size = 4;
     private int[] array;
     private int element;
 
+
     public SmartArray() {
         array = new int[size];
     }
+
 
     public void add(int element) {
         if (array.length == size) {
@@ -24,6 +27,7 @@ public class SmartArray {
             this.element++;
     }
 
+
     public int get(int index) {
         for (int i = 0; i < array.length; i++) {
             if (i == index) {
@@ -33,24 +37,43 @@ public class SmartArray {
         return 0;
     }
 
-    public int remove(int index) {
+
+    public void remove(int index) {
+        int[] newArray = new int[array.length - 1];
+        int[] firstPartArray = new int[index];
+        int[] secondPartArray = new int[newArray.length - 1];
+        boolean isEndOfArray = false;
         for (int i = 0; i < array.length; i++) {
-            if (i == index) {
-                int[] newArray = new int[array.length - 1];
-                for (int j = 0; j < newArray.length; j++) {
-                    newArray[j] = array[j];
+            if (index < array.length - 1) {
+                if (i == index - 1) {
+                    System.arraycopy(array, 0, firstPartArray, 0, firstPartArray.length);
+                    if (index != array.length - 1) {
+                        for (int j = 0; j < array.length - 2; j++) {
+                            secondPartArray[j] = array[j + index + 1];
+                        }
+                    } else {
+                        isEndOfArray = true;
+                        break;
+                    }
                 }
-                array = newArray;
             }
         }
-        return 0;
+        if (!isEndOfArray) {
+            System.arraycopy(firstPartArray, 0, newArray, 0, firstPartArray.length);
+            System.arraycopy(secondPartArray, 0, newArray, 0 + index, secondPartArray.length);
+            array = newArray;
+        } else {
+            array = firstPartArray;
+        }
     }
+
 
     public void forEach(Consumer<Integer> consumer) {
         for (int j : array) {
             consumer.accept(j);
         }
     }
+
 
     public boolean contains(int element) {
         for (int j : array) {
@@ -61,21 +84,31 @@ public class SmartArray {
         return false;
     }
 
-    // Adds element at the specific index, the element at this index gets shifted
-    // to the right alongside any following elements, increasing the size
 
     public void add(int index, int element) {
+        int[] newArray = new int[array.length - index];
         if (index >= 0 && index < array.length - 1) {
             for (int i = 0; i < array.length; i++) {
                 if (i == index) {
-                    int[] newArray = new int[array.length - index];
                     for (int j = 0; j < newArray.length; j++) {
-                        array[i] = newArray[j];
+                        newArray[j] = array[j + index];
                     }
                     array[i] = element;
+                    break;
                 }
             }
+        } else {
+            newArray[newArray.length - 1] = element;
         }
+        int[] finalArray = new int[array.length + 1];
+        for (int i = 0; i < array.length; i++) {
+            finalArray[i] = array[i];
+        }
+        int beginIndex = finalArray.length - newArray.length;
+        for (int i = 0; i < newArray.length; i++) {
+            finalArray[i + beginIndex] = newArray[i];
+        }
+        array = finalArray;
     }
 
 }
