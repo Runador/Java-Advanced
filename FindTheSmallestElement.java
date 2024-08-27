83e5a118b8c6d5e77196e5f9bf3a80d448c4470b
Runador <runador123456@gmail.com>
2024-08-27 23:55:57 +0300

    Adding FindTheSmallestElement.java

---
+++ FindTheSmallestElement.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FindTheSmallestElement {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt).collect(Collectors.toList());
+
+        System.out.println(getMinIndex(numbers));
+
+    }
+
+    public static int getMinIndex(List<Integer> numbers) {
+        int index = 0;
+        int min = Integer.MAX_VALUE;
+        for (int i = 0; i < numbers.size(); i++) {
+            int number = numbers.get(i);
+            if (number < min) {
+                min = number;
+                index = i;
+            }
+        }
+        return index;
+    }
+
+}
