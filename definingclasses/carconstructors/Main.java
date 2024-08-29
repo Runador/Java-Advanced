2c9c9fc8c4a30936cbf00e33e30cf62b8319a3f7
Runador <runador123456@gmail.com>
2024-08-29 22:05:12 +0300

    Adding carconstructors

---
+++ definingclasses/carconstructors/Main.java
@@ -0,0 +1,18 @@
+package definingclasses.carconstructors;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String brand = scanner.nextLine();
+        Car car1 = new Car(brand);
+        System.out.println(car1.carInfo());
+        String model = scanner.nextLine();
+        int horsePower = Integer.parseInt(scanner.nextLine());
+        Car car2 = new Car(brand, model, horsePower);
+        System.out.println(car2.carInfo());
+
+    }
+}
