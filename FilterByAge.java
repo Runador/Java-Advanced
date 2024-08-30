31cfd8a8d5f2497656b5630c5ff16f4b17ee2c68
Runador <runador123456@gmail.com>
2024-08-30 23:57:01 +0300

    Modifying files

--- FilterByAge.java
+++ FilterByAge.java
@@ -1,4 +1,54 @@
-package functionalProgrammingLab;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class FilterByAge {
+
+    private static class Person {
+        String name;
+        int age;
+
+        public Person(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        List<Person> personList = new ArrayList<>();
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        while (n-- > 0) {
+            String[] data = scanner.nextLine().split(", ");
+            String name = data[0];
+            int age = Integer.parseInt(data[1]);
+            Person person = new Person(name, age);
+            personList.add(person);
+        }
+
+        String ageCriteria = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        String format = scanner.nextLine();
+
+        Predicate<Person> predicate = getAgeCriteria(ageCriteria, age);
+
+        switch (format) {
+            case "name" -> personList.stream().filter(predicate).forEach(p -> System.out.println(p.name));
+            case "age" -> personList.stream().filter(predicate).forEach(p -> System.out.println(p.age));
+            case "name age" -> personList.stream().filter(predicate).forEach(p -> System.out.printf("%s - %d%n", p.name, p.age));
+        }
+    }
+
+    private static Predicate<Person> getAgeCriteria(String ageCriteria, int age) {
+        return switch (ageCriteria) {
+            case "younger" -> person -> person.age <= age;
+            case "older" -> person -> person.age >= age;
+            default -> throw new IllegalArgumentException("Unsupported input value");
+        };
+    }
 }
--- PredicateParty.java
+++ PredicateParty.java
@@ -1,10 +1,41 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.function.Function;
 
 public class PredicateParty {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        List<String> names = Arrays.stream(scanner.nextLine().split("\\s+")).toList();
 
+        String input = scanner.nextLine();
+
+        while (!input.equals("Party!")) {
+            String[] data = input.split("\\s");
+            if (input.equals("Double")) {
+                String command = data[1];
+                String subString = data[2];
+                Function<List<String>, List<String>> functionDouble = stringList -> stringList.stream()
+                        .filter(s -> s.length() == Integer.parseInt(subString)).map(stringList.add())
+                switch (command) {
+                    case "StartsWith" -> names.stream().filter(e -> e.startsWith(subString))
+                            .map()
+                    )
+                    case "EndsWith" -> names.removeIf(e -> e.endsWith(subString));
+                    case "Length" -> names.removeIf(e -> e.length() == Integer.parseInt(subString));
+                }
+            } else if (input.equals("Remove")) {
+                String command = data[1];
+                String subString = data[2];
+                switch (command) {
+                    case "StartsWith" -> names.removeIf(e -> e.startsWith(subString));
+                    case "EndsWith" -> names.removeIf(e -> e.endsWith(subString));
+                    case "Length" -> names.removeIf(e -> e.length() == Integer.parseInt(subString));
+                }
+            }
+            input = scanner.nextLine();
+        }
 
     }
 }
