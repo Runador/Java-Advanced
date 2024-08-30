3b953834a87e58d6ed03cfb658a4a71375480a59
Runador <runador123456@gmail.com>
2024-08-31 02:37:47 +0300

    Modifyinf PredicateParty.java

--- PredicateParty.java
+++ PredicateParty.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Function;
@@ -8,34 +9,59 @@ public class PredicateParty {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        List<String> names = Arrays.stream(scanner.nextLine().split("\\s+")).toList();
+        List<String> names = Arrays.stream(scanner.nextLine().split("\\s+"))
+                        .collect(Collectors.toList());
 
         String input = scanner.nextLine();
 
         while (!input.equals("Party!")) {
             String[] data = input.split("\\s");
-            if (input.equals("Double")) {
-                String command = data[1];
+            String command1 = data[0];
+            if (command1.equals("Double")) {
+                String command2 = data[1];
                 String subString = data[2];
-                Function<List<String>, List<String>> functionDouble = stringList -> stringList.stream()
-                        .filter(s -> s.length() == Integer.parseInt(subString))
-                        .collect(Collectors.toList());
-                switch (command) {
-                    case "StartsWith" -> names.stream().filter(e -> e.startsWith(subString))
-                    case "EndsWith" -> names.removeIf(e -> e.endsWith(subString));
-                    case "Length" -> names.removeIf(e -> e.length() == Integer.parseInt(subString));
+                switch (command2) {
+                    case "StartsWith" -> {
+                        String name = names.stream().filter(e -> e.startsWith(subString)).findFirst().orElse(null);
+                        long counter = names.stream().filter(e -> e.startsWith(subString)).count();
+                        for (int i = 0; i < counter; i++) {
+                            names.add(name);
+                        }
+                    }
+                    case "EndsWith" -> {
+                        String name = names.stream().filter(e -> e.endsWith(subString)).findFirst().orElse(null);
+                        long counter = names.stream().filter(e -> e.endsWith(subString)).count();
+                        for (int i = 0; i < counter; i++) {
+                            names.add(name);
+                        }
+                    }
+                    case "Length" -> {
+                        String name = names.stream().filter(e -> e.length() == Integer.parseInt(data[2])).findFirst().orElse(null);
+                        long counter = names.stream().filter(e -> e.length() == Integer.parseInt(data[2])).count();
+                        for (int i = 0; i < counter; i++) {
+                            names.add(name);
+                        }
+                    }
                 }
-            } else if (input.equals("Remove")) {
+            } else if (command1.equals("Remove")) {
                 String command = data[1];
                 String subString = data[2];
-                switch (command) {
-                    case "StartsWith" -> names.removeIf(e -> e.startsWith(subString));
-                    case "EndsWith" -> names.removeIf(e -> e.endsWith(subString));
-                    case "Length" -> names.removeIf(e -> e.length() == Integer.parseInt(subString));
+                if (!names.isEmpty()) {
+                    switch (command) {
+                        case "StartsWith" -> names.removeIf(name -> name.startsWith(subString));
+                        case "EndsWith" -> names.removeIf(e -> e.endsWith(subString));
+                        case "Length" -> names.removeIf(e -> e.length() == Integer.parseInt(subString));
+                    }
                 }
             }
             input = scanner.nextLine();
         }
-
+        Collections.sort(names);
+        if (!names.isEmpty()) {
+            System.out.print(String.join(", ", names));
+            System.out.println(" are going to the party!");
+        } else {
+            System.out.println("Nobody is going to the party!");
+        }
     }
 }
