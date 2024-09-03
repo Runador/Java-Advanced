3b6f212c06f977b2d0dfe4c9847eccab8821e8ce
Runador <runador123456@gmail.com>
2024-09-03 23:20:21 +0300

    Adding carsalesman

---
+++ carsalesman/Car.java
@@ -0,0 +1,49 @@
+package carsalesman;
+
+public class Car {
+
+    private String model;
+    private Engine engine;
+    private int weight;
+    private String color;
+
+    public Car(String model, Engine engine, int weight, String color) {
+        this.model = model;
+        this.engine = engine;
+        this.weight = weight;
+        this.color = color;
+    }
+
+    public Car(String model, )
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
---
+++ carsalesman/Engine.java
@@ -0,0 +1,11 @@
+package carsalesman;
+
+public class Engine {
+
+    private String model;
+    private int power;
+    private int displacement;
+    private String efficiency;
+
+
+}
